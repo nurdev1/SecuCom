e6fc8ad2f78ef6882628bfdeebbbb52d04f88e24
nurdev1 <fatoudevweb1@gmail.com>
2022-11-22 15:47:32 +0000

    first commit

---
+++ src/main/java/com/SecuCom/SecuCom/SecuComApplication.java
@@ -0,0 +1,13 @@
+package com.SecuCom.SecuCom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuComApplication.class, args);
+	}
+
+}
